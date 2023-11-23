9d3f9aa0de64f3166e25cc81782e55d4a36ca574
luismasters <97652871+luismasters@users.noreply.github.com>
2023-11-23 00:02:50 -0300

    cambios pg inicio

--- WebApplication1/Default.aspx.designer.cs
+++ WebApplication1/Default.aspx.designer.cs
@@ -13,23 +13,5 @@ namespace WebApplication1
 
     public partial class _Default
     {
-
-        /// <summary>
-        /// Control UpdatePanel1.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.UpdatePanel UpdatePanel1;
-
-        /// <summary>
-        /// Control rptArticulos.
-        /// </summary>
-        /// <remarks>
-        /// Campo generado automáticamente.
-        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Repeater rptArticulos;
     }
 }
